05c452fe3c5be6d01af9b98b3306bd9a3c622bc6
Franco Ferrufino <ferrufinofranco@hotmail.com>
2026-06-01 22:23:20 -0300

    Carrito - Logica de agregar productos al carrito

---
+++ Services/CarritoService.cs
@@ -0,0 +1,24 @@
+﻿using EcommerceImportados.Data;
+
+namespace EcommerceImportados.Services
+{
+    public class CarritoService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CarritoService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AgregarProducto(int productoId)
+        {
+
+        }
+
+        public decimal ObtenerTotal()
+        {
+            return 0m;
+        }
+    }
+}
