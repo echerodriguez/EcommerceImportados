c873f6986ea37788a87f514c2b058f059d5ab818
eche <echerodriguez@gmail.com>
2026-06-08 21:10:27 -0300

    Productos - Se agregan filtros por categoria y orden por precio

---
+++ Controllers/EnvioController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace EcommerceImportados.Controllers
+{
+    public class EnvioController : Controller
+    {
+        public IActionResult Index(int pedidoId)
+        {
+            ViewBag.PedidoId = pedidoId;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Continuar(int pedidoId)
+        {
+            return RedirectToAction(
+                "Pagar",
+                "Pagos",
+                new { pedidoId });
+        }
+    }
+}
